7910c74181a647760608cd7034d80c29c40e6693
Dmitry Andreev <dimaryavi1@yandex.ru>
2018-09-30 21:58:52 +0300

    First Version

---
+++ MyString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyString.MyString str = new MyString.MyString("Привет ");
+            MyString.MyString str1 = new MyString.MyString("как дета");
+            str = str + str1;
+            str.Replace("ет","ел");
+            Console.WriteLine(str);
+        }
+    }
